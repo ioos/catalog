6e88fec6be559faf951beec56d8aeb6e12b09c35
Micah Wengren <micah.wengren@gmail.com>
2016-12-15 08:39:28 -0500

    Added bash script for NDBC THREDDS catalog WAF generation

---
+++ waf_generation_scripts/hf_radar_dac/hf_radar_ndbc_waf.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python WAF_generator.py --waf_dir=/var/www/html/mwengren/hf_radar_dac --catalog_url=http://sdf.ndbc.noaa.gov/thredds/catalog.xml
+#python WAF_generator.py --waf_dir=waf --catalog_url=http://sdf.ndbc.noaa.gov/thredds/catalog.xml
