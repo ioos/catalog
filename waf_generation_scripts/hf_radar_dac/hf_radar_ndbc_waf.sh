2d9e45c740f9369a95b99be62aa973e4ee17ee16
Micah Wengren <micah.wengren@gmail.com>
2017-11-21 15:46:11 -0500

    Updated NDBC THREDDS URL.  Cleaned shell scripts up a little

--- waf_generation_scripts/hf_radar_dac/hf_radar_ndbc_waf.sh
+++ waf_generation_scripts/hf_radar_dac/hf_radar_ndbc_waf.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-python WAF_generator.py --waf_dir=/var/www/html/mwengren/hf_radar_dac_ndbc --catalog_url=http://sdf.ndbc.noaa.gov/thredds/catalog.xml
-#python WAF_generator.py --waf_dir=waf --catalog_url=http://sdf.ndbc.noaa.gov/thredds/catalog.xml
+python WAF_generator.py --waf_dir=/var/www/html/mwengren/hf_radar_dac_ndbc --catalog_url=http://dods.ndbc.noaa.gov/thredds/hfradar.xml
---
+++ waf_generation_scripts/hf_radar_dac/hf_radar_ndbc_waf_local.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python WAF_generator.py --waf_dir=waf --catalog_url=http://dods.ndbc.noaa.gov/thredds/hfradar.xml
